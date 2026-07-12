6dcb946ff9c67531ca9d4f7f1a91fd1cb70b6424
Lostinfrequencies369 <helloankitmalik@gmail.com>
2026-07-12 20:28:58 +0530

    Remove duplicate frontend loader from config

--- config.js
+++ config.js
@@ -1,12 +1,4 @@
-/* Public Apps Script endpoint used by the GitHub Pages viewer. */
+/* Public Google Apps Script endpoint used by the GitHub Pages viewer. */
 window.VET_TX_CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbxgXG-bhNG3Td3oJzdnmorqvx1fRM5daWD_QDagHzqQeSVTC0GGXaDS0xNJIiiz_U8IdA/exec'
 };
-
-/* Load the new multi-route poster directory interface. */
-(function () {
-  var script = document.createElement('script');
-  script.src = 'directory-app.js?v=20260712-1';
-  script.async = false;
-  document.head.appendChild(script);
-})();
