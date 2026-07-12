879635923599a679b9c5df5b08c527aafbb24be6
Lostinfrequencies369 <helloankitmalik@gmail.com>
2026-07-12 15:23:48 +0530

    Add Apps Script API configuration

---
+++ config.js
@@ -0,0 +1,8 @@
+/*
+ * Paste the deployed Google Apps Script /exec URL below.
+ * Example: https://script.google.com/macros/s/AKfycb.../exec
+ * Use only the base public /exec URL without query parameters.
+ */
+window.VET_TX_CONFIG = {
+  API_URL: 'PASTE_YOUR_APPS_SCRIPT_WEB_APP_EXEC_URL_HERE'
+};
